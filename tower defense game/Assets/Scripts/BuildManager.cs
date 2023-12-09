6925a7eda8b6ee4e09f6530c601f9372e6c55a98
jewelypoo <jewelxjocelyn@gmail.com>
2023-12-08 16:29:42 -0800

    implemented currency

--- tower defense game/Assets/Scripts/BuildManager.cs
+++ tower defense game/Assets/Scripts/BuildManager.cs
@@ -24,18 +24,30 @@ public class BuildManager : MonoBehaviour
 
 
     //public bool (property) that checks if nodes are able to be built on
-public bool CanBuild { get { return cannonToBuild != null; } }
+    public bool CanBuild { get { return cannonToBuild != null; } }
 
- public void SelectCannonToBuild (TurretBlueprint cannon)
+    public void SelectCannonToBuild(TurretBlueprint cannon)
     {
         cannonToBuild = cannon;
     }
 
-    public void BuildCannonOn (Node node)
+    public void BuildCannonOn(Node node)
     {
+        //checks if player has enough money for the cannon
+        if (PlayerStats.Money < cannonToBuild.cost)
+        {
+            Debug.Log("Not enough money!");
+            return;
+
+        }
+        //subtracts players money from how much it cost to build turret
+        PlayerStats.Money -= cannonToBuild.cost;
+
         GameObject cannon = (GameObject)Instantiate(cannonToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
         node.cannon = cannon;
 
+        //displays new currency in log
+        Debug.Log("Cannon Built. Money Left: " + PlayerStats.Money); 
 
     }
 
---
+++ tower defense game/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    public static int Money;
+    public int startMoney = 300;
+
+
+    public void Start()
+    {
+        Money = startMoney;
+
+    }
+
+
+
+}
